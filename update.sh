7d4aafe149d5554e06ac08e9f847580ca6e805bc
Evans Tucker <evans.tucker@gmail.com>
2019-11-14 08:39:58 -0800

    Helm 3 is official!

--- update.sh
+++ update.sh
@@ -12,14 +12,15 @@ echo "ENV HELM_DIFF_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url
 echo "ENV HELM_GIT_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/aslafy-z/helm-git/releases/latest))"
 echo "ENV HELM_PUSH_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/chartmuseum/helm-push/releases/latest))"
 echo "ENV HELM_SECRETS_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/futuresimple/helm-secrets/releases/latest))"
-echo "ENV HELM_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/helm/helm/releases/latest))"
 
-# Helm 3 is still in pre-release state.
-echo "ENV HELM3_VERSION=v3.0.0-rc.2"
+# Helm 2 is no longer the latest release. Fixing this to the most recent v2 release.
+echo "ENV HELM_VERSION=v2.16.1"
+
+echo "ENV HELM3_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/helm/helm/releases/latest))"
 
 # This is pre-release, there is no latest tag yet.
 #echo "ENV K9S_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/derailed/k9s/releases/latest))"
-echo "ENV K9S_VERSION=0.9.1"
+echo "ENV K9S_VERSION=0.9.3"
 
 echo "ENV KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 echo "ENV SOPS_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/mozilla/sops/releases/latest))"
