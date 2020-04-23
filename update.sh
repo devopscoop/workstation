efe1a8dee7dc89f32551a781f29dae0f6a6453d6
Evans Tucker <evans.tucker@gmail.com>
2020-04-23 14:23:08 -0700

    Updating

--- update.sh
+++ update.sh
@@ -11,7 +11,7 @@ echo "ENV FLUXCTL_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}'
 
 # Manually setting version number. I didn't see an easy way to find latest version, and I don't care to spend that much time on this.
 # Find the latest version here: https://cloud.google.com/sdk/docs/quickstart-linux
-echo "ENV GOOGLE_CLOUD_SDK_VERSION=284.0.0"
+echo "ENV GOOGLE_CLOUD_SDK_VERSION=290.0.0"
 
 echo "ENV HELMFILE_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/roboll/helmfile/releases/latest))"
 echo "ENV HELM_2TO3_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/helm/helm-2to3/releases/latest))"
@@ -19,15 +19,8 @@ echo "ENV HELM_DIFF_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url
 echo "ENV HELM_GIT_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/aslafy-z/helm-git/releases/latest))"
 echo "ENV HELM_PUSH_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/chartmuseum/helm-push/releases/latest))"
 echo "ENV HELM_SECRETS_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/zendesk/helm-secrets/releases/latest))"
-echo
-cat <<EOF
-# The Helm guys keep flipping the "latest" release between Helm 2 and Helm 3,
-# so you have to set the HELM2 and HELM3 VERSION variables manually.
-# It's currently set to: $(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/helm/helm/releases/latest))
-EOF
-echo "ENV HELM2_VERSION=v2.16.5"
-echo "ENV HELM3_VERSION=v3.1.2"
-echo
+echo "ENV HELM2_VERSION=$(curl -s https://github.com/helm/helm/releases | grep -oE '/helm/helm/releases/tag/v[^"]*' | grep -oE 'v2[^"]*' | head -n 1)"
+echo "ENV HELM3_VERSION=$(curl -s https://github.com/helm/helm/releases | grep -oE '/helm/helm/releases/tag/v[^"]*' | grep -oE 'v3[^"]*' | head -n 1)"
 echo "ENV K9S_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/derailed/k9s/releases/latest))"
 echo "ENV KUBECTL_VERSION=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
 echo "ENV SOPS_VERSION=$(basename $(curl -s -o /dev/null -w '%{redirect_url}' https://github.com/mozilla/sops/releases/latest))"
