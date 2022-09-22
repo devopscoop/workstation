207971d5ea4b312bfad795afd9eb37b219da0a62
Evans Tucker <gmail@evanstucker.com>
2022-09-22 08:54:00 -0700

    Removing unnecessary sudo and moving alpine bits to a subdirectory.

---
+++ alpine-deprecated/README.md
@@ -0,0 +1,3 @@
+AWS has not released a musl-compatible version of the latest aws-cli, so I had to deprecate this image. The new workstation image is using Ubuntu.
+
+See this issue for more details: https://github.com/aws/aws-cli/issues/4685
--- azure.sh
+++ azure.sh
@@ -2,13 +2,13 @@
 
 # https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+apt-get update
+apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+    tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get update
-sudo apt-get install azure-cli
+    tee /etc/apt/sources.list.d/azure-cli.list
+apt-get update
+apt-get install azure-cli
